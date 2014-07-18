d68362c7c002a5db62e13f745ad2ca7e28ab207e
Theo <bbtfrr@gmail.com>
2014-07-18 10:12:44 +0800

    remove test/dummy rails app, use fake environment.rb instead

--- lib/tasks/redundancy_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :redundancy do
-#   # Task goes here
-# end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,21 +4,18 @@ ENV["RAILS_ENV"] = "test"
 require 'minitest/autorun'
 # require 'pry-rescue/minitest'
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require 'support/environment.rb'
+require 'redundancy'
+require 'rails/test_help'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
-Dir[
-  "#{File.dirname(__FILE__)}/support/**/*.rb", 
-  "#{File.dirname(__FILE__)}/dummy/app/models/*.rb"
-  ].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("/dummy/test/fixtures", __FILE__)
-end
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+
 # Load fixtures from the engine
 class ActiveSupport::TestCase
   fixtures :all
